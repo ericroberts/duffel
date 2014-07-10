254c4e9a21dfb4d632f344ec7448ad3007193727
Eric Roberts <ericroberts@gmail.com>
2014-07-10 18:01:15 -0400

    Add Duffel and its tests.

--- lib/duffel.rb
+++ lib/duffel.rb
@@ -1,5 +1,18 @@
 require "duffel/version"
 
 module Duffel
-  # Your code goes here...
+  def self.method_missing(method, *args, &block)
+    fetch_default = lambda do |key|
+      raise KeyError.new("key not found: #{key}")
+    end
+
+    define_singleton_method(method) do |options=(args.first || {})|
+      return_value = options.to_h.fetch(:fallback, fetch_default)
+      fallback = return_value.is_a?(Proc) ? return_value : lambda { |key| return_value }
+
+      env_name = method.to_s.upcase
+      ENV.fetch(env_name, &fallback)
+    end
+    self.send(method)
+  end
 end
---
+++ spec/lib/duffel_spec.rb
@@ -0,0 +1,40 @@
+require 'duffel'
+
+describe Duffel do
+  it 'should not raise an error' do
+    expect { subject }.to_not raise_error
+  end
+
+  describe '.not_an_existing_method' do
+    context 'without a matching environment variable' do
+      it 'should raise an error if nothing exists in the environment' do
+        expect { subject.not_an_existing_method }.to raise_error(KeyError)
+      end
+
+      context 'with a fallback passed' do
+        it 'should return the fallback' do
+          expect(
+            subject.another_non_existing_method(fallback: 'blah')
+          ).to eq 'blah'
+        end
+
+        context 'fallback is nil' do
+          it 'should return nil' do
+            expect(
+              subject.yet_another_non_existing_method(fallback: nil)
+            ).to eq nil
+          end
+        end
+      end
+    end
+
+    context 'with a matching environment variable' do
+      before { expect(ENV).to receive(:fetch).with('AN_EXISTING_METHOD').and_return(response) }
+      let(:response) { 'response' }
+
+      it 'should return the env variable' do
+        expect(subject.an_existing_method).to eq response
+      end
+    end
+  end
+end
